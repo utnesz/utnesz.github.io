10df31c7e5c1b4900bfb3262a1b4c849857f5179
utnesz <90829509+utnesz@users.noreply.github.com>
2023-01-27 14:19:21 +0100

    JSClock upload

---
+++ JSClock/scripts.js
@@ -0,0 +1,70 @@
+const hourLmnt = document.querySelector('.hour')
+const minuteLmnt = document.querySelector('.minute')
+const secondLmnt = document.querySelector('.second')
+const timeLmnt = document.querySelector('.time')
+const dateLmnt = document.querySelector('.date')
+const toggleLmnt = document.querySelector('.toggle')
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+toggleLmnt.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+    timeLmnt.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}`: minutes}`
+    dateLmnt.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers (answer154)
+
+const scale = (num, inMin, inMax, outMin, outMax) => {
+    return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+setTime();
+
+setInterval(setTime, 1000)
